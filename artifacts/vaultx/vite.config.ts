dff44eb834d27f0c135daca1b943a6b9e21995e7
wggg01-dev <wggg01@gmail.com>
2026-04-30 06:41:32 +0000

    Changed BASEPATH

--- artifacts/vaultx/vite.config.ts
+++ artifacts/vaultx/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/vaultx/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
